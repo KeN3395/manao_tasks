a367f4232693a8e04b8b7e002e4f54aa8773bd73
KeN3395 <ken.trey94@gmail.com>
2017-04-21 17:23:45 +0300

    Update tasks.php

--- tasks.php
+++ tasks.php
@@ -4,7 +4,8 @@
  $i = 0;
  while($n>0){
    $k= $n%10;
-   if ($k<5) $i++;
+   if ($k<5) 
+	$i++;
    $n=(int)($n/10);
  }
  echo $i;
@@ -20,7 +21,8 @@ for($i = 1000; $i < 10000; $i++){
         $sum = $sum + $k;
         $n = (int)($n / 10);
     }
-    if ($sum == $s) echo ($i."\n");
+    if ($sum == $s) 
+	echo ($i."\n");
 }
 ?>
 //3. Выяснить, образуют ли цифры данного натурального числа N возрастающую последовательность.
@@ -38,7 +40,8 @@ $flag = true;
         }
         $n =(int)($n / 10);
     }
-    if($flag) echo ("Образует");
+    if($flag)
+	echo ("Образует");
 ?>
 //4. Найти все четные четырехзначные числа, цифры которых следуют в порядке возрастания или убывания.
 <?php
@@ -47,7 +50,8 @@ $flag = true;
 		while($n >=10){
 			$k = (int)($n % 10);
 			$l = (int)(($n / 10) % 10);
-			if($k <= $l) return false;
+			if($k <= $l) 
+				return false;
 			$n =(int)($n / 10);
 		}
 		return $flag;
@@ -57,13 +61,15 @@ $flag = true;
 		while($n >=10){
 			$k = (int)($n % 10);
             		$l = (int)(($n / 10) % 10);
-			if($k >= $l) return false;
+			if($k >= $l) 
+				return false;
             		$n =(int)($n / 10);
 		}
 		return $flag;
 	}
 	for($i = 1000; $i < 10000; $i += 2){
-		if(Up($i) || Down($i)) echo $i."\n";
+		if(Up($i) || Down($i))
+			echo $i."\n";
 	}
 ?>
 //5. По заданному натуральному числу N получить число M, записанное цифрами исходного числа, взятыми в обратном порядке.
@@ -97,7 +103,93 @@ for($i = 1000; $i < 10000; $i++){
         }
         $n = (int)($n / 10);
     }
-   if($flag)echo ($i."\n");
+   if($flag)
+	echo ($i."\n");
 }
 ?>
 //7. Выяснить, есть ли в записи натурального числа N две одинаковые цифры.
+<?php
+$n = 1234;
+if (duplicate($n))
+    echo "присутствуют";
+else
+    echo "отсутствуют";
+function duplicate($num)
+{
+    while ($num != 0) {
+        $n = $num;
+        $count = 0;
+        while ($n != 0) {
+            if ($n % 10 == $num % 10)
+                $count++;
+            $n = (int)($n/10);
+        }
+        if($count >= 2)
+            return true;
+        $num = (int)($num / 10);
+    }
+    return false;
+}
+?>
+//8. Получить все четырехзначные целые числа, в записи которых нет одинаковых цифр.
+<?php
+for($i = 1000; $i < 10000; $i++){
+    if(!duplicate($i)) 
+	echo $i."\t";
+}
+function duplicate($num)
+{
+    while ($num != 0) {
+        $n = $num;
+        $count = 0;
+        while ($n != 0) {
+            if ($n % 10 == $num % 10)
+                $count++;
+            $n = (int)($n/10);
+        }
+        if($count >= 2)
+            return true;
+        $num = (int)($num / 10);
+    }
+    return false;
+}
+?>
+//9. Дано  натуральное  число  N.  Определить,  является  ли  оно  автоморфным. Автоморфное число  равно последним разрядам квадрата этого числа. Например,  5 и 25,  6 и 36,  25 и 625.
+<?php
+$n = 25;
+if (automorphic($n))
+    echo "является";
+else
+    echo "не является";
+function number($num)
+{
+    $i = 1;
+    while ($num != 0) {
+        $i *= 10;
+        $num = (int)($num / 10);
+    }
+    return $i;
+}
+function automorphic($num)
+{
+    return ($num * $num) % number($num) == $num;
+}    
+?>
+//10. Найти все меньшие N числа-палиндромы, которые при возведении в квадрат дают палиндром. Число называется палиндромом, если его запись читается одинаково с начала и с конца.
+<?php
+$n = 1000;
+for ($i = 0; $i < $n; $i++)
+    if (isPalindrome($i * $i) == $i * $i)
+        echo "$i \t";
+function isPalindrome($n)
+{
+    $m = 0;
+    while($n != 0)     
+    {
+        $m *= 10;   
+        $m += $n % 10;
+        $n =(int)($n / 10);
+    }   
+    return $m;
+} 
+?>
